6f6873329bc0125631fcb951141491541064b79f
Qazi Sajid Azam <azam.qsa@gmail.com>
2018-05-28 23:40:36 +0530

    Update sound_recorder.py

--- sound_recorder.py
+++ sound_recorder.py
@@ -14,13 +14,13 @@ audio = pyaudio.PyAudio()
 stream = audio.open(format=FORMAT, channels=CHANNELS,
                 rate=RATE, input=True,
                 frames_per_buffer=CHUNK)
-print "recording..."
+print ("recording...")
 frames = []
  
 for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
     data = stream.read(CHUNK)
     frames.append(data)
-print "finished recording"
+print ("finished recording")
  
  
 # stop Recording
